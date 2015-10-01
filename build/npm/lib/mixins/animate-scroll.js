7149dffd7d412a53ed646b19f8b4084b06c50111
David Todd <dtodd@gradifi.com>
2015-10-01 10:08:36 -0400

    Add timestamp to requestAnimationFrame fallback.

--- build/npm/lib/mixins/animate-scroll.js
+++ build/npm/lib/mixins/animate-scroll.js
@@ -27,7 +27,7 @@ var currentWindowProperties = function() {
 var requestAnimationFrame = (function () {
   return  currentWindowProperties() ||
           function (callback, element, delay) {
-              window.setTimeout(callback, delay || (1000/60));
+              window.setTimeout(callback, delay || (1000/60), new Date().getTime());
           };
 })();
 
