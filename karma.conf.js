961696a407eff94ed073513c77acb0fa71d4f34e
Joachim Karlsson <Jockkee_@hotmail.com>
2015-10-17 00:05:42 +0200

    added mocha and karma with webpack as test foundation.

---
+++ karma.conf.js
@@ -0,0 +1,38 @@
+var webpack = require('webpack')
+
+module.exports = function (config) {
+
+  config.set({
+
+    browsers: [ 'Chrome' ],
+    frameworks: [ 'mocha' ],
+    reporters: [ 'mocha' ],
+
+    files: [
+      'tests.webpack.js'
+    ],
+
+    preprocessors: {
+      'tests.webpack.js': [ 'webpack' ]
+    },
+
+    webpack: {
+      devtool: 'inline-source-map',
+      module: {
+        loaders: [
+          { test: /\.js$/, exclude: /node_modules/, loader: 'babel' }
+        ]
+      },
+      plugins: [
+        new webpack.DefinePlugin({
+          'process.env.NODE_ENV': JSON.stringify('test')
+        })
+      ]
+    },
+
+    webpackServer: {
+      noInfo: true
+    }
+  })
+
+}
