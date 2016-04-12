37108dafa0f1e97480ae13a1c2487e41cc60b822
Danil Pismenny <danil@brandymint.ru>
2016-04-12 21:05:23 +0300

    give 6 elements in page-test

--- modules/__tests__/page-test.js
+++ modules/__tests__/page-test.js
@@ -3,9 +3,10 @@ import { render, unmountComponentAtNode } from 'react-dom'
 import Rtu      from 'react-addons-test-utils'
 import React    from 'react'
 /* Components to test */
-import Element  from '../components/Element.js';
-import Link     from '../components/Link.js';
-import events   from '../mixins/scroll-events.js';
+import Element    from '../components/Element.js';
+import Link       from '../components/Link.js';
+import DirectLink from '../components/DirectLink.js';
+import events     from '../mixins/scroll-events.js';
 /* Test */
 import expect   from 'expect'
 import assert   from 'assert';
@@ -27,12 +28,14 @@ describe('Page', () => {
           <li><Link to="test3" spy={true} smooth={true} duration={scrollDuration}>Test 3</Link></li>
           <li><Link to="test4" spy={true} smooth={true} duration={scrollDuration}>Test 4</Link></li>
           <li><Link to="test5" spy={true} smooth={true} duration={scrollDuration}>Test 5</Link></li>
+          <li><DirectLink to="test6" spy={true} smooth={true} duration={scrollDuration}>Test 6</DirectLink></li>
         </ul>
         <Element name="test1" className="element">test 1</Element>
         <Element name="test2" className="element">test 2</Element>
         <Element name="test3" className="element">test 3</Element>
         <Element name="test4" className="element">test 4</Element>
         <Element name="test5" className="element">test 5</Element>
+        <Element name="test6" className="element">test 6</Element>
       </div>
 
   beforeEach(() => {
@@ -44,15 +47,15 @@ describe('Page', () => {
     unmountComponentAtNode(node)
   });
     
-  it('renders five elements of link/element', (done) => {
+  it('renders six elements of link/element', (done) => {
 
     render(component, node, () => {
 
         var allLinks = node.querySelectorAll('a');
         var allTargets = node.querySelectorAll('.element');
         
-        expect(allLinks.length).toEqual(5);
-        expect(allTargets.length).toEqual(5);
+        expect(allLinks.length).toEqual(6);
+        expect(allTargets.length).toEqual(6);
 
         done();
 
