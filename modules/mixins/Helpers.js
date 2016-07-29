1eb2048153b7c4a5025e5b65cef6d2ee9eb98333
李雁楠 <yannan.li@ele.me>
2016-07-29 16:18:55 +0800

    BUGFIX: fix the bug that links does't work when the last 2 or 3 elements(on the bottom) are in a view;

--- modules/mixins/Helpers.js
+++ modules/mixins/Helpers.js
@@ -23,7 +23,11 @@ var protoTypes = {
   defaultActive: React.PropTypes.bool
 };
 
-var needActive = false;
+// 保存最近一次点击的 link
+var clickLink = null;
+
+// 保存 scrollContainer
+var scrollSpyContainer = null;
 
 var Helpers = {
 
@@ -59,26 +63,12 @@ var Helpers = {
         if (event.stopPropagation) event.stopPropagation();
         if (event.preventDefault) event.preventDefault();
 
-        // change the active Link at first(do not depend on the element's scroll)
-        // var self = this;
-        // scroller.setActiveLink(void 0);
-        // this.setState({
-        //   active : false
-        // }, function () {
-        //   scrollSpy.updateStates();
-        //   needActive = true;
+        clickLink = this.props.to;
 
-        //   scroller.setActiveLink(this.props.to);
-        //   self.setState({
-        //     active : true
-        //   }, function () {
-        //     scrollSpy.updateStates()
-        //   });
+        // TODO 取消之前的事件并停止滚动!
 
-        //   if(self.props.onSetActive) {
-        //     self.props.onSetActive(self.props.to);
-        //   }
-        // });
+        // 在底部时, 触发 scroll 事件(否则不会切换 activeLink)
+        scrollSpyContainer.scrollTop -= 1;
 
         /*
          * do the magic!
@@ -102,7 +92,6 @@ var Helpers = {
         if (isOutside && activeLink === to) {
           scroller.setActiveLink(void 0);
           this.setState({ active : false });
-
         } else if (isInside && activeLink != to) {
           scroller.setActiveLink(to);
           this.setState({ active : true });
@@ -115,14 +104,32 @@ var Helpers = {
         }
       },
 
+      // 判断是否当前滚动条已到底部
+      isBottom: function (){
+        var contentElements = scrollSpyContainer.children;
+        return (scrollSpyContainer.clientHeight + 1) >= contentElements[contentElements.length - 1].getBoundingClientRect().bottom;
+      },
+
+
+      // 判断 link 对应的 element 是否全部可视
+      isExposed: function (link){
+        if (!link) return false;
+
+        var activeElement = scroller.get(link);
+        var cords = activeElement.getBoundingClientRect();
+        var topHeight = cords.top;// element 顶部距离 container 的距离
+        var bottomHeight = cords.bottom;// element 底部距离 container 的距离
+
+        // 假如顶部低于 container 顶部,而且底部高于 container 底部
+        return (Math.floor(topHeight) + 1 >= 0 && Math.floor(bottomHeight) <= scrollSpyContainer.clientHeight);
+      },
+
       componentDidMount: function() {
 
-        var containerId = this.props.containerId;
 
-        var scrollSpyContainer = containerId ? document.getElementById(containerId) : document;
+        scrollSpyContainer = scrollSpyContainer || ( this.props.containerId ? document.getElementById(this.props.containerId) : document );
 
         scrollSpy.mount(scrollSpyContainer);
-
 
         if(this.props.spy) {
           var to = this.props.to;
@@ -138,8 +145,6 @@ var Helpers = {
 
           var spyHandler = function(y) {
 
-            // if (needActive) return true;
-
             if(!element || this.props.isDynamic) {
                 element = scroller.get(to);
                 if(!element){ return;}
@@ -150,14 +155,29 @@ var Helpers = {
             }
 
             var offsetY = y - this.props.offset;
-            var isInside = (offsetY >= Math.floor(elemTopBound) && offsetY <= Math.floor(elemBottomBound));
+            var isInside = (offsetY >= Math.floor(elemTopBound) && offsetY <= Math.floor(elemBottomBound));// 判断是否当前元素已接触到上边界(逻辑有问题: 假如是底部的,没法接触上边界,怎么办?)
             var isOutside = (offsetY < Math.floor(elemTopBound) || offsetY > Math.floor(elemBottomBound));
             var activeLink = scroller.getActiveLink();
+
+            // 判断当前 activeLink 是否全部可视
+            var isActiveLinkExposed = this.isExposed(clickLink);
+
+            // 假如此时已处于底部, 而且 clickLink 的全部内容都可视, 那么设置 clickLink
+            // to 为当前 link 对应的 element
+            if (this.isBottom() && isActiveLinkExposed) {
+              // 假如命中, 那么设为 active 状态
+              if (to === clickLink) {
+                scroller.setActiveLink(clickLink);
+                this.setState({ active : true });
+                scrollSpy.updateStates();
+
+                return true;
+              }
+            };
 
             if (isOutside && activeLink === to) {
               scroller.setActiveLink(void 0);
               this.setState({ active : false });
-
             } else if (isInside && activeLink != to) {
               scroller.setActiveLink(to);
               this.setState({ active : true });
@@ -167,7 +187,6 @@ var Helpers = {
               }
 
               scrollSpy.updateStates();
-
             }
           }.bind(this);
 
@@ -191,6 +210,10 @@ var Helpers = {
       componentWillUnmount: function() {
         scrollSpy.unmount();
         animateScroll.resetContainer();
+
+        // 清除闭包缓存
+        clickLink = null;
+        scrollSpyContainer = null;
       },
       render: function() {
 
