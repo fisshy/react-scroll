4595c069715fbc32e026e15489f0f401afb06b0f
Joachim Karlsson <joachim@causam.se>
2017-04-18 08:52:20 +0200

    removed build and published v1.5.1

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 var gulp = require('gulp');
 
-var SOURCE = './modules/**/*.js';
-var DEST = './build/npm/lib'
+var SOURCE_PACKAGE_JSON = './package.json';
+var DEST_PACKAGE_JSON = './build/npm'
 
 var SOURCE_README = '*.md';
 var DEST_README = './build/npm';
 
 gulp.task('default', function() {
-	gulp.src(SOURCE)
-  	.pipe(gulp.dest(DEST));
+	gulp.src(SOURCE_PACKAGE_JSON)
+  	.pipe(gulp.dest(DEST_PACKAGE_JSON));
 
   	gulp.src(SOURCE_README)
   	.pipe(gulp.dest(DEST_README));
