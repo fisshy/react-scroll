2e824c0a7a9a030768d1298d77baee205a468cef
Andrew Hutchings <andrew.hutchings@gmail.com>
2017-06-14 04:30:04 -0400

    Use JSX instead of React DOM factories to fix React warning in >= 15.6 (#224)
    Fixes the following warning in React 15.6 and above:
    
    “Warning: Accessing factories like React.DOM.a has been deprecated and will be removed in v16.0+. Use the react-dom-factories package instead.  Version 1.0 provides a drop-in replacement. For more info, see https://fb.me/react-dom-factories”

--- modules/components/Button.js
+++ modules/components/Button.js
@@ -5,7 +5,11 @@ var Helpers = require('../mixins/Helpers');
 
 class Button extends React.Component{
   render() {
-    return React.DOM.input(this.props, this.props.children);
+    return (
+      <input {...this.props}>
+        {this.props.children}
+      </input>
+    );
   }
 };
 
