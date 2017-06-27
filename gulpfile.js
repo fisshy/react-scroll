ed521e22772e5d5aa83d60206485ac0386c071fa
sromain@talend.com <8hyrmetlI8!t>
2017-06-27 12:22:35 +0200

    test for pr

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,13 @@ var gulp = require('gulp');
 
 var SOURCE = './modules/**/*.js';
 var DEST = './build/npm/lib'
-
 var SOURCE_README = '*.md';
 var DEST_README = './build/npm';
 
-gulp.task('default', function() {
+gulp.task('default', function () {
 	gulp.src(SOURCE)
-  	.pipe(gulp.dest(DEST));
+		.pipe(gulp.dest(DEST));
 
-  	gulp.src(SOURCE_README)
-  	.pipe(gulp.dest(DEST_README));
+	gulp.src(SOURCE_README)
+		.pipe(gulp.dest(DEST_README));
 });
