e0edf79508dc5d563d0db07148b2407c99b51d9d
Malthe Jørgensen <malthe.jorgensen@gmail.com>
2017-07-01 11:44:24 -0700

    [Fix] <Element>: Remove `parentBindings` on `<div>` warning

--- modules/components/Element.js
+++ modules/components/Element.js
@@ -5,8 +5,14 @@ var Helpers = require('../mixins/Helpers');
 
 class Element extends React.Component{
   render() {
+    // Remove `parentBindings` from props
+    let newProps = Object.assign({}, this.props);
+    if (newProps.parentBindings) {
+      delete newProps.parentBindings;
+    }
+
     return (
-      <div {...this.props} ref={(el) => { this.props.parentBindings.domNode = el; }}>
+      <div {...newProps} ref={(el) => { this.props.parentBindings.domNode = el; }}>
         {this.props.children}
       </div>
     );
