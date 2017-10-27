b7741adea65897654dbb0e543216765cea7e6be3
Sean Lee <freshdried@gmail.com>
2017-10-26 21:22:41 -0400

    solution: bind cancel event listener once during module initialization time

--- modules/mixins/cancel-events.js
+++ modules/mixins/cancel-events.js
@@ -3,5 +3,4 @@ const events = ['mousedown', 'mousewheel', 'touchmove', 'keydown']
 
 module.exports = {
   subscribe : (cancelEvent) => (typeof document !== 'undefined') && events.forEach(event => addPassiveEventListener(document, event, cancelEvent)),
-  unsubscribe : (cancelEvent) => (typeof document !== 'undefined') && events.forEach(event => removePassiveEventListener(document, event, cancelEvent))
 };
