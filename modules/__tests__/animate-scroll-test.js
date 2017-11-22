c276bbe7e05c1e5ca9d83a895b2491260bfdfb86
Nikolai Kim <ru.yaruson@gmail.com>
2017-11-22 17:13:54 +0300

    Add failing test with two DIVs

--- modules/__tests__/animate-scroll-test.js
+++ modules/__tests__/animate-scroll-test.js
@@ -16,6 +16,7 @@ var currentPositionY = function () {
 describe('AnimateScroll', () => {
 
   let node;
+  let node2;
   const duration = 10;
   const waitDuration = duration * 10;
 
@@ -27,18 +28,31 @@ describe('AnimateScroll', () => {
       <div style={{ height: '10000px' }}></div>
     </div>
 
+  let tallComponent2 =
+    <div id="hugeComponent2">
+      <a onClick={() => animateScroll.scrollToTop()}>Scroll To Top!</a>
+      <a onClick={() => animateScroll.scrollTo(100)}>Scroll To 100!</a>
+      <a onClick={() => animateScroll.scrollMore(10)}>Scroll More!</a>
+      <div style={{ height: '10000px' }}></div>
+    </div>
+
   beforeEach(() => {
     node = document.createElement('div');
+    node2 = document.createElement('div');
     document.body.appendChild(node);
+    document.body.appendChild(node2);
   })
 
   afterEach(function () {
     window.scrollTo(0, 0);
     node.style.cssText = "";
+    node2.style.cssText = "";
     document.body.style.cssText = "";
 
     unmountComponentAtNode(node);
+    unmountComponentAtNode(node2);
     document.body.removeChild(node);
+    document.body.removeChild(node2);
     document.body.innerHtml = "";
   });
 
@@ -156,5 +170,25 @@ describe('AnimateScroll', () => {
         done();
       }, 150);
     });
-  })
+  });
+
+  it('can scroll two DIVs', (done) => {
+    render(tallComponent, node, () => {
+      render(tallComponent2, node2, () => {
+        window.scrollTo(0, 0);
+        node.style.cssText = "position: fixed; top: 0; bottom: 200px; width 100%; overflow: scroll";
+        node2.style.cssText = "position: fixed; top: 0; bottom: 200px; width 100%; overflow: scroll";
+        document.body.style.cssText = "overflow: hidden;";
+
+        animateScroll.scrollTo(300, { duration: duration, container: node });
+        animateScroll.scrollTo(400, { duration: duration, container: node2 });
+        setTimeout(() => {
+          expect(node.scrollTop).toEqual(300);
+          expect(node2.scrollTop).toEqual(400);
+          done();
+        }, waitDuration);
+      });
+    });
+  });
+
 });
