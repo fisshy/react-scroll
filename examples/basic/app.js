5a2427a717aef292425ea2f26af313048648f08e
Joachim Karlsson <Jockkee_@hotmail.com>
2019-06-12 08:18:31 +0200

    updated example with offset

--- examples/basic/app.js
+++ examples/basic/app.js
@@ -3,7 +3,7 @@
 import React     from 'react';
 import ReactDOM  from 'react-dom';
 
-import { Link, DirectLink, Element , Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
+import { Link, DirectLink, Element , Events, animateScroll, scrollSpy, scroller } from 'react-scroll'
 
 const durationFn = function(deltaTop) {
     return deltaTop;
@@ -30,11 +30,12 @@ class Section extends React.Component{
   scrollToTop() {
     scroll.scrollToTop();
   }
-  scrollTo() {
+  scrollTo(offset) {
     scroller.scrollTo('scroll-to-element', {
       duration: 800,
       delay: 0,
-      smooth: 'easeInOutQuart'
+      smooth: 'easeInOutQuart',
+      offset: offset
     })
   }
   scrollToWithContainer() {
@@ -74,19 +75,21 @@ class Section extends React.Component{
             <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
               <ul className="nav navbar-nav">
                 <li><Link activeClass="active" className="test1" to="test1" spy={true} smooth={true} duration={500} >Test 1</Link></li>
-                <li><Link activeClass="active" className="test2" to="test2" spy={true} smooth={true} duration={500}>Test 2</Link></li>
+                <li><Link activeClass="active" className="test2" to="test2" spy={true} smooth={true} duration={500} offset={-55}>Test 2</Link></li>
                 <li><Link activeClass="active" className="test3" to="test3" spy={true} smooth={true} duration={500} >Test 3</Link></li>
                 <li><Link activeClass="active" className="test4" to="test4" spy={true} smooth={true} duration={500}>Test 4</Link></li>
                 <li><Link activeClass="active" className="test5" to="test5" spy={true} smooth={true} duration={500} delay={1000}>Test 5 ( delay )</Link></li>
                 <li><Link activeClass="active" className="test6" to="anchor" spy={true} smooth={true} duration={500}>Test 6 (anchor)</Link></li>
                 <li><Link activeClass="active" className="test7" to="test7" spy={true} smooth={true} duration={durationFn}>Test 7 (duration and container)</Link></li>
-                <li> <a onClick={() => scroll.scrollTo(100)}>Scroll To 100!</a></li>
-                <li> <a onClick={() => scroll.scrollToBottom()}>Scroll To Bottom</a></li>
-                <li> <a onClick={() => scroll.scrollMore(500)}>Scroll 500 More!</a></li>
-                <li> <a onClick={() => scroll.scrollMore(1000, { delay : 1500 })}>Scroll 1000 More! ( delay ) </a></li>
+                <li> <a onClick={() => animateScroll.scrollTo(100)}>Scroll To 100!</a></li>
+                <li> <a onClick={() => animateScroll.scrollTo(100)}>Scroll To 100+-50offset!</a></li>
+                <li> <a onClick={() => animateScroll.scrollToBottom()}>Scroll To Bottom</a></li>
+                <li> <a onClick={() => animateScroll.scrollMore(500)}>Scroll 500 More!</a></li>
+                <li> <a onClick={() => animateScroll.scrollMore(1000, { delay : 1500 })}>Scroll 1000 More! ( delay ) </a></li>
                 <li><Link activeClass="active" className="test8" to="same" spy={true} smooth={true} duration={500}>Same target</Link></li>
                 <li><Link activeClass="active" className="test9" to="same" spy={true} smooth={true} duration={500}>Same target</Link></li>
                 <li><a className="test1" to="test1" onClick={() => this.scrollTo()} >Scroll to element</a></li>
+                <li><a className="test1" to="test1" onClick={() => this.scrollTo(-50)} >Scroll to element (offset -50)</a></li>
                 <li><a className="test1" to="test1" onClick={() => this.scrollToWithContainer()} >Scroll to element within container</a></li>
               </ul>
             </div>
@@ -97,7 +100,7 @@ class Section extends React.Component{
           test 1
         </Element>
 
-        <Element name="test2" className="element">
+        <Element name="test2" className="element no-padding">
           test 2
         </Element>
 
