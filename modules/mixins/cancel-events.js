7aae9537ecc5d58c808ad7f8799005782879eca2
Linas <69507024+pankeku@users.noreply.github.com>
2023-11-28 16:07:32 +0200

    modify cancel events list (#570)
    Co-authored-by: Linas Danisevicius <linas.danisevicius@devbridge.com>

--- modules/mixins/cancel-events.js
+++ modules/mixins/cancel-events.js
@@ -1,5 +1,5 @@
 import { addPassiveEventListener, removePassiveEventListener } from './passive-event-listeners';
-const events = ['mousedown', 'mousewheel', 'touchmove', 'keydown']
+const events = ['mousedown', 'wheel', 'touchmove', 'keydown']
 
 export default {
   subscribe : (cancelEvent) => (typeof document !== 'undefined') && events.forEach(event => addPassiveEventListener(document, event, cancelEvent)),
